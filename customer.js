853f6949fd4a9f6eca7151244ff9544644d90bfe
Liam Cavens <liamcavens@googlemail.com>
2018-06-08 17:51:39 +0100

    added collection values

--- customer.js
+++ customer.js
@@ -4,4 +4,28 @@ let Customer = function(name, wallet){
     this.recordCollection = [];
 }
 
+Customer.prototype.buyRecordFromShop = function(record, store){
+    if(this.wallet >= record.price){
+    this.wallet -= record.price;
+    this.recordCollection.push(record);
+    store.sellRecord(record);
+}};
+
+Customer.prototype.getCollectionValue = function(){
+
+    return this.recordCollection.reduce(function(acc, record){
+        return acc += record.price;
+    }, 0)
+}
+
+Customer.prototype.getGenreValue = function(genre){
+
+    return this.recordCollection.reduce(function(acc, record){
+        if( record.genre === genre){
+          return acc += record.price;
+        }
+        return acc;
+    }, 0)
+}
+
 module.exports = Customer;
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -17,6 +17,7 @@ describe("Record", function() {
         record1 = new Record("Mumford & Sons", "Babel", "Country", 10);
         record2 = new Record("Elton John", "Goodbye Yellow Brick Road", "Indie", 12);
         record3 = new Record("Funky BeeJays", "Gettin' Busy", "Jazz", 7);
+        customer1 = new Customer("Upul", 140);
 
         store.addRecord(record1);
         store.addRecord(record2);
@@ -30,7 +31,7 @@ describe("Record", function() {
         assert.strictEqual(record1.info(), "Artist: Mumford & Sons, Title: Babel, Genre: Country, Price: 10.");
     });
 
-    it("Should be able to list records in inventory", function(){
+    it("Should be able to list records in inventory", function(){        
         assert.deepStrictEqual(store.showRecords(), [record1.info(), record2.info()]);
     });
 
@@ -45,5 +46,30 @@ describe("Record", function() {
 
     it("Should be able to return records by genre", function(){
         assert.deepStrictEqual(store.getByGenre("Indie"), [record2])
+    })
+
+    it("Customer should be able to buy records, wallet is also affected", function(){
+        customer1.buyRecordFromShop(record1, store);
+        assert.strictEqual(customer1.recordCollection.length, 1);
+        assert.strictEqual(customer1.wallet, 130)
+    })
+
+    it("Customer cannot buy record if funds less than price", function(){
+        customer1.wallet = 5;
+        customer1.buyRecordFromShop(record1, store);
+        assert.strictEqual(customer1.recordCollection.length, 0);
+    });
+
+    it("Customer should be able to get value of collection", function(){
+        customer1.buyRecordFromShop(record1, store);
+        customer1.buyRecordFromShop(record2, store);
+        assert.strictEqual(customer1.recordCollection.length, 2);
+        assert.strictEqual(customer1.getCollectionValue(), 22)
+    })
+
+    it("Customer should be able to get value of genre in hs collection", function(){
+        customer1.buyRecordFromShop(record1, store);
+        customer1.buyRecordFromShop(record1, store);
+        assert.strictEqual(customer1.getGenreValue("Country"), 20)
     })
 })
