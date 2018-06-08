f372bf4fec7ad6abdf1f7bfc90f805285ca425d7
Liam Cavens <liamcavens@googlemail.com>
2018-06-08 17:15:59 +0100

    added more functions and tests

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -15,18 +15,31 @@ describe("Record", function() {
     beforeEach(function() {
         store = new Store("Upuls Mad Music Emporium", "Motherwell", 500);
         record1 = new Record("Mumford & Sons", "Babel", "Country", 10);
-        record2 = new Record("Elton John", "Goodbye Yellow Brick Road", 12);
-        record3 = new Record("Funky BeeJays", "Gettin' Busy", 7);
+        record2 = new Record("Elton John", "Goodbye Yellow Brick Road", "Indie", 12);
+        record3 = new Record("Funky BeeJays", "Gettin' Busy", "Jazz", 7);
 
         store.addRecord(record1);
         store.addRecord(record2);
     });
 
     it("Should start with two records in inventory", function(){
-        assert.strictEqual(store.inventory.length, 2)
-    })
+        assert.strictEqual(store.inventory.length, 2);
+    });
 
     it("Should be able to show record info", function(){
-        assert.strictEqual(record1.info(), "Artist: Mumford & Sons, Title: Babel, Genre: Country, Price: 10.")
+        assert.strictEqual(record1.info(), "Artist: Mumford & Sons, Title: Babel, Genre: Country, Price: 10.");
+    });
+
+    it("Should be able to list records in inventory", function(){
+        assert.deepStrictEqual(store.showRecords(), [record1.info(), record2.info()]);
+    });
+
+    it("Should be able to sell record and store balance goes up", function(){
+        store.sellRecord(record1);
+        assert.strictEqual(store.balance, 510);
     })
+
+    it("Should be able to show stor financial status", function(){
+        assert.strictEqual(store.showFinancialStatus(), 522);
+    });
 })
--- store.js
+++ store.js
@@ -9,4 +9,23 @@ Store.prototype.addRecord = function(record){
     this.inventory.push(record);
 }
 
+Store.prototype.showRecords = function(){
+    return this.inventory.map(function (record){
+        return record.info();
+    });
+}
+
+Store.prototype.sellRecord = function(record){
+    this.balance += record.price;
+}
+
+Store.prototype.showFinancialStatus = function(){
+    let financialBalance = this.balance;
+
+    this.inventory.forEach(function(record){
+        financialBalance += record.price;
+    });
+    return financialBalance;
+}
+
 module.exports = Store;
